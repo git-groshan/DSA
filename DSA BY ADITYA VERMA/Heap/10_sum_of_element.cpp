ef145588aa71434782b6208787bf8d4fb175e85f
Roshan <roshangupta2020@gmail.com>
2024-02-14 19:38:25 +0530

    Completed AV HEAP playlist

--- DSA BY ADITYA VERMA/Heap/10_sum_of_element.cpp
+++ DSA BY ADITYA VERMA/Heap/10_sum_of_element.cpp
@@ -14,7 +14,29 @@ using namespace std;
  traverse the array and check if(num>k1Smallest and num<k2Smallest) ans+=num
 */
 
+int kthSmallestElement(vector<int>&v , int k){
+    priority_queue<int>maxHeap;
+    int i=0;
+    while(i<v.size()){
+        maxHeap.push(v[i]);
+        if(maxHeap.size() > k)maxHeap.pop();
+        i++;
+    }
+    return maxHeap.top();
+}
+
+
 int main(){
+
+    vector<int>v={1,3,12,5,15,11};
+    int k1 = 3 , k2 = 6;
+    int k1thSmallest = kthSmallestElement(v , k1);
+    int k2thSmallest = kthSmallestElement(v , k2);
+    int sum = 0;
+    for(int i=0;i<v.size();i++){
+        if(v[i]>k1thSmallest and v[i]<k2thSmallest) sum+=v[i];
+    }
+    cout<<"Sum of elements in between is "<<sum<<endl;
 
 return 0;
 }
